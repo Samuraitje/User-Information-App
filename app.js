53c8955982785087a090e883c4d37e65b2affa01
Sam <sam.jing92@gmail.com>
2017-11-17 17:59:29 +0100

    third commit

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.get('/', (req, res) => {
 	}); 
 });
 
+//Part 0 route 1: renders a page that displays all the users.
+//The http method get will request the users page with the corresponding render response of 'users' and the parsed data.
+//From the core module fs the readFile function is used to read the file, after the file is read, a callback function will be invoked to handle the error and parsing the JSON file.
+//The userData will be parsed by JSON.parse(userData) and stored in the Let parsedData.
+
 app.get('/users', (req, res) => {
 		fs.readFile("allusers.json", (err, userData) => {
 			if (err) {
@@ -27,6 +32,9 @@ app.get('/users', (req, res) => {
 	});
 });		
 
+
+//Part 1 route 2: renders a page that displays a form which is your search bar.
+//The http method get will request the search page with the corresponding render response of 'search' and the searchresult array from the http post method. 
 app.get('/search', (req, res) => {
 	res.render('search', {
 		title: 'Search Bar',
@@ -34,6 +42,13 @@ app.get('/search', (req, res) => {
 	});
 });
 
+//Part 1 route 2: takes in the post request from a form and displays the matching users on a new page. Matching the firstname and lastname.
+//This http post method will gather a submission from the user and search through the allusers json file to receive the desired result.
+//It includes a similar callback function as route 1 that handles errors and parsing the userData.
+//The JSON file is filterd by using a forEach function and if statement that compares the first name and last name with the search input.
+//The search input is requested by using the req.body.search that is enabled by the module body-parser.
+//The matched results will be stored in the array searchResult.
+//Finally the page search will be rendert will the searchResult object.
 app.post('/search', (req, res) => {
 	fs.readFile("allusers.json", (err, userData) => {
 		if (err) {
@@ -53,18 +68,26 @@ app.post('/search', (req, res) => {
 	});
 });
 
+
+//Part 2 route 4: renders a page with a form with three inputs on it (firstname, lastname and email) that allows you to add new users to the allusers.json file.
+//The http method get renders route to the adduser page. That has a form with the 3 input bars.
 app.get('/adduser', (req, res) => {
 	res.render('adduser', {
 		title: 'Add User'
 	});
 });
 
+//Part 2 route 5: takes in the post request from the 'create user' form, then adds the user to the allusers.josn file. And redirects the user to the route that displays all the users.
+//The http method post will gather all the user inputs from the adduser form to the let userFile.
+//First the JSON file is read by the function readFile and afterwards new inputs for creating a new user will be pushed to the let userFile.
+//Subsequently, the writeFile function is used to write that file to the allusers json file and also needs changed back to a json string by JSON.stringify.
+//Finally, the user will be redirected to the users page.
 app.post('/adduser', (req, res) => {
 	fs.readFile("allusers.json", (err, userData) => {
 		if (err) {
 			console.log('File not found!')
 		}
-		var userFile = JSON.parse(userData);
+		let userFile = JSON.parse(userData);
   	userFile.push({ firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email }); 
   	fs.writeFile("allusers.json", JSON.stringify(userFile), function(err){
     	if (err) throw err;
@@ -75,6 +98,6 @@ app.post('/adduser', (req, res) => {
 });
 
 //Initiated a local server on port 3000
-app.listen(5000, () => {
+app.listen(1000, () => {
     console.log('listening');
 });
